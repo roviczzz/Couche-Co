cb03d87954caf79bd14f9d29c9bfee66e1fd9c01
divinoken <dkc0262@dlsud.edu.ph>
2025-06-23 22:41:55 +0800

    Updated_Privacy.cshtml
    Changed to Hihi.

--- Couche-SysIntegFO/Couche-SysIntegFO/obj/Debug/net8.0/Couche-SysIntegFO.AssemblyInfo.cs
+++ Couche-SysIntegFO/Couche-SysIntegFO/obj/Debug/net8.0/Couche-SysIntegFO.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Couche-SysIntegFO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4e85795e3e852d7b61c2e2f1dda7efd3a38991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78feee8c3299fa01aaf54187449a83e177ea424a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Couche-SysIntegFO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Couche-SysIntegFO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
